7d2ff3fd5760bf5aa9b39430956eb94d1ffc63ef
troingle <bugsinsidemyhead@gmail.com>
2023-09-21 18:17:44 +0300

    Links, pages etc

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-document.getElementById('redirectButton').addEventListener('click', function() {
+document.getElementById('aboutRedirect').addEventListener('click', function() {
     window.location.href = 'https://troingle.github.io/about';
+});
+
+document.getElementById('projectsRedirect').addEventListener('click', function() {
+    window.location.href = 'https://troingle.github.io/projects';
+});
+
+document.getElementById('readingList').addEventListener('click', function() {
+    window.location.href = 'https://troingle.github.io/lukulista';
+});
+
+document.getElementById('itch').addEventListener('click', function() {
+    window.location.href = 'https://troingle.itch.io';
+});
+
+document.getElementById('discordo').addEventListener('click', function() {
+    window.location.href = 'https://discord.com/users/717988279596613682';
 });
