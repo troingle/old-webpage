ee69081363545cfa155f8b077f68ec328056f11c
troingle <bugsinsidemyhead@gmail.com>
2023-09-21 18:23:30 +0300

    Links

--- script.js
+++ script.js
@@ -17,3 +17,7 @@ document.getElementById('itch').addEventListener('click', function() {
 document.getElementById('discordo').addEventListener('click', function() {
     window.location.href = 'https://discord.com/users/717988279596613682';
 });
+
+document.getElementById('back').addEventListener('click', function() {
+    window.location.href = 'https://troingle.github.io';
+});
