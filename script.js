87af624dc1baffbcbeb2b33f18ddd655a0568139
troingle <bugsinsidemyhead@gmail.com>
2023-09-28 21:00:46 +0300

    Trying out changing text

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// To be used
+const textElement = document.getElementById('huge-text');
+const texts = ['Text 1', 'Text 2', 'Another creative text name'];
+let currentIndex = 0;
+
+function changeText() {
+    textElement.style.opacity = 0;
+    setTimeout(() => {
+        textElement.textContent = texts[currentIndex];
+        currentIndex = (currentIndex + 1) % texts.length;
+        textElement.style.opacity = 1;
+    }, 500);
+}
+
+setInterval(changeText, 5000);
+changeText();
