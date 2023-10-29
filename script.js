d1c10aaa9ae29d0128a4e888cd40e3f91ade7360
troingle <bugsinsidemyhead@gmail.com>
2023-10-29 08:17:38 +0000

    Added clickable links (?)

--- script.js
+++ script.js
@@ -2,6 +2,35 @@ const textElement = document.getElementById('changing-text');
 const texts = ['Welcome', 'Tervetuloa', 'Willkommen', "Välkommen", "いらっしゃいませ"];
 let currentIndex = 0;
 
+var box1 = document.getElementById('box1');
+var box2 = document.getElementById('box2');
+var box3 = document.getElementById('box3');
+var box4 = document.getElementById('box4');
+var box5 = document.getElementById('box5');
+var box6 = document.getElementById('box6');
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    box1.addEventListener('click', function (event) {
+        window.location.href = 'https://www.example.com';
+    });
+    box2.addEventListener('click', function (event) {
+        window.location.href = 'https://www.example.com';
+    });
+    box3.addEventListener('click', function (event) {
+        window.location.href = 'https://www.example.com';
+    });
+    box4.addEventListener('click', function (event) {
+        window.location.href = 'https://www.example.com';
+    });
+    box5.addEventListener('click', function (event) {
+        window.location.href = 'https://www.example.com';
+    });
+    box6.addEventListener('click', function (event) {
+        window.location.href = 'https://www.example.com';
+    });
+});
+
 function changeText() {
     textElement.style.opacity = 0;
     setTimeout(() => {
