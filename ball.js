a4f1de74d5bcf7aa5a4d8f18498383dad84a272d
troingle <bugsinsidemyhead@gmail.com>
2023-10-29 08:37:35 +0000

    Separated files

---
+++ ball.js
@@ -0,0 +1,18 @@
+
+const ball = document.getElementById("ball");
+const centerX = window.innerWidth / 2;
+const centerY = window.innerHeight / 2;
+const radius = 100;
+const speed = 2 * Math.PI / 1000;
+let angle = 0;
+const x = centerX + radius * Math.cos(angle);
+const y = centerY + radius * Math.sin(angle);
+
+ball.style.left = x + "px";
+ball.style.top = y + "px";
+
+angle += speed;
+
+requestAnimationFrame(moveBall);
+
+
