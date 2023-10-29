af9cce5316fd98532429db7d74c11f5dd94e1a66
troingle <bugsinsidemyhead@gmail.com>
2023-10-29 15:15:30 +0000

    Added request for next ball animation frame

--- ball.js
+++ ball.js
@@ -1,18 +1,18 @@
-
 const ball = document.getElementById("ball");
 const centerX = window.innerWidth / 2;
 const centerY = window.innerHeight / 2;
 const radius = 100;
 const speed = 2 * Math.PI / 1000;
 let angle = 0;
-const x = centerX + radius * Math.cos(angle);
-const y = centerY + radius * Math.sin(angle);
 
-function moveBall(){
+function moveBall() {
+    const x = centerX + radius * Math.cos(angle);
+    const y = centerY + radius * Math.sin(angle);
     ball.style.left = x + "px";
-    ball.style.top = y + "px";x
-
+    ball.style.top = y + "px";
     angle += speed;
+
+    requestAnimationFrame(moveBall);
 }
 
 requestAnimationFrame(moveBall);
